ec3ae04da315aad4ef66805617f1a85bf7e51c9a
Bryce <bsaito88@gmail.com>
2016-07-17 03:47:05 -1000

    Indexing works.

--- indexing.sql
+++ indexing.sql
@@ -3,6 +3,7 @@ DROP USER IF EXISTS indexed_cars_user;
 CREATE USER indexed_cars_user;
 CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
+
 \i scripts/car_models.sql;
 \i scripts/car_model_data.sql;
 -- \i scripts/car_model_data.sql;
@@ -15,18 +16,23 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 -- \i scripts/car_model_data.sql;
 -- \i scripts/car_model_data.sql;
 
-SELECT DISTINCT ON (make_title) make_title, now()
+\timing
+
+-- DROP INDEX IF EXISTS some_index;
+CREATE INDEX some_index ON car_models (make_code, make_title, model_code, model_title, year);
+
+SELECT DISTINCT ON (make_title) make_title
   FROM car_models WHERE make_code = 'LAM';
 
-SELECT DISTINCT ON (model_title) make_title, now()
+SELECT DISTINCT ON (model_title) make_title
   FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 
 SELECT make_code, model_code, model_title, year
   FROM car_models
   WHERE make_code = 'LAM';
 
-SELECT * FROM car_models, now()
+SELECT * FROM car_models
   WHERE year BETWEEN 2010 AND 2015;
 
-SELECT * FROM car_models, now()
+SELECT * FROM car_models
   WHERE year = 2010
